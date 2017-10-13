dfe333ae55628119b8b58e62f381c5d63c2c4f84
PauliGall <dianpau01@gmail.com>
2017-10-13 16:02:40 -0500

    actualización 4

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,8 @@ Rails.application.routes.draw do
   get 'espacios/salaPosg'
   post'espacios/salaPosg'
 
+  get 'solicituds/new'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
