69ef34123eba6da52f43bb81ed2b5d0f30e4de86
PauliGall <dianpau01@gmail.com>
2017-10-27 14:31:13 -0500

    A6

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,13 @@
 class WelcomeController < ApplicationController
   def show
- 
-render '/welcome/solicitud'
+
 
   end
+
+  def login
+    solicituds[:usuario_id] = nil
+    solicituds[:admin] = nil
+    @usuario = Usuarios.new
+  end
+
 end
