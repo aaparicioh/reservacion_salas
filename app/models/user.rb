8d2f63aff754646842e939e3d13d363532aa41ab
Axel Aparicio Hernández <axel.ah@ceiich.unam.mx>
2018-06-29 13:55:47 -0500

    Cambios en algoritmo de colisiones

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :reservacions
   has_many :videoconferencium
-    
 
   def self.create_new_user(params)
   @user = User.create!(params)
